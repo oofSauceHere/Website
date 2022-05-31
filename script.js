7019c75f65491ea7ae0377613f60936851d01239
Deccan Maniam <dev.oofSauce@gmail.com>
2022-05-31 00:52:49 -0400

    Fixed spinny pfp + changed action button functionality

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let i = 1;
 function scroll() {
-    document.getElementById('about').classList.add('scroll');
+    if(i === 1) {
+        document.getElementById('about').style.transform = "translateY(-100%)";
+    } else {
+        document.getElementById('about').style.transform = "translateY(100%)";
+    }
+
+    i *= -1;
+}
+
+let j = 360;
+function rotatePFP() {
+    document.getElementById('blurb-img').style.transform = "rotate(" + j + "deg)";
+    j += 360;
 }
 
 document.getElementById('action-btn').addEventListener('click', scroll);
+document.getElementById('blurb-img').addEventListener('click', rotatePFP);
