643f988f5dea1218a32bc59569f01f931d7fd518
Deccan Maniam <dev.oofSauce@gmail.com>
2022-06-08 00:18:25 -0400

    Working on projects page

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 /* this entire thing is scuffed */
+document.getElementById('action-btn').style.pointerEvents = "none";
+setTimeout(() => {
+    document.getElementById('action-btn').style.pointerEvents = "auto";
+}, 1000);
 
 let i = 1;
 function scroll() {
+    document.getElementById('action-btn').style.pointerEvents = "none";
     if(i === 1) {
-        document.getElementById('about').style.transform = "translateY(-100%)";
+        // document.getElementById('about').style.transform = "translateY(-100%)";
+        document.getElementById('projects').style.transform = "translateY(-100%)";
 
         document.getElementById('side').style.transform = "translateX(100%)";
         document.getElementById('side').style.visibility = "hidden";
 
-        document.getElementById('nav').style.visibility = "visible";
-        document.getElementById('nav').style.transform = "translateX(-5vw)";
+        setTimeout(() => {
+            document.getElementById('nav').style.visibility = "visible";
+            document.getElementById('nav').style.transform = "translateX(-5vw)";
+        }, 1000);
     } else {
-        document.getElementById('about').style.transform = "translateY(100%)";
-
-        document.getElementById('side').style.visibility = "visible";
-        document.getElementById('side').style.transform = "translateX(0%)";
+        // document.getElementById('about').style.transform = "translateY(100%)";
+        document.getElementById('projects').style.transform = "translateY(100%)";
 
         document.getElementById('nav').style.transform = "translateX(0%)";
-        document.getElementById('nav').style.visibility = "hidden";
+        setTimeout(() => {
+            document.getElementById('nav').style.visibility = "hidden";
+        }, 500);
+
+        setTimeout(() => {
+            document.getElementById('side').style.visibility = "visible";
+            document.getElementById('side').style.transform = "translateX(0%)";
+        }, 500);
     }
+
+    setTimeout(() => {
+        document.getElementById('action-btn').style.pointerEvents = "auto";
+    }, 1500);
 
     i *= -1;
 }
